be4079875e723840f595d25ad52f72334c2fc019
My Project <mk2536495@gmail.com>
2025-05-22 10:40:56 +0530

    user login done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -10,5 +10,11 @@ router.post('/register',[
 ],
    userController.registerUser
 )
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Password must be filled'),
+],
+   userController.loginUser
+)
 
 module.exports=router;
